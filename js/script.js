cccf8bfc0b37dcee3210dab6c2f7a367b4db8258
Abid Al-Wasi <abidalwasi@outlook.com>
2021-04-12 23:54:57 +0600

    updated project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 window.onload = () => {
   const word = document.getElementById("word");
+  
+  const getBtn = document.getElementById("getBtn");
+  const postBtn = document.getElementById("postBtn");
+  getBtn.addEventListener("click", getData);
+  postBtn.addEventListener("click", sendData);
+  
   function getData() {
-    
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://reqres.in/api/users");
+  }
+  
+  function sendData() {
+  
   }
 }
